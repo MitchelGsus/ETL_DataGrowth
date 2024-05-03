08125437261376bd590b4ff62120ce9f69b8003b
Mitchel Nuñez <u20245202@utp.edu.pe>
2024-05-03 00:00:48 -0500

    add env var

--- prueba.py
+++ prueba.py
@@ -1,1 +1,10 @@
-print('Hola Mundo')
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ZOOM_ACCOUNT_ID = os.getenv('ZOOM_ACCOUNT_ID')
+ZOOM_CLIENT_ID = os.getenv('ZOOM_CLIENT_ID')
+ZOOM_CLIENT_SECRET = os.getenv('ZOOM_CLIENT_SECRET')
+
+print(ZOOM_ACCOUNT_ID)
