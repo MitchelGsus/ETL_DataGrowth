f021853486e9e8252c6d84f9a5fd4a3a92a70d01
Mitchel Nuñez <mitchelingsus@gmail.com>
2024-05-03 19:28:31 -0500

    feat: very features

--- zoom.py
+++ zoom.py
@@ -2,6 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 import pandas as pd
+from datetime import datetime
 
 ## Token is = 
 load_dotenv()
@@ -11,11 +12,22 @@ ZOOM_CLIENT_ID = os.getenv('ZOOM_CLIENT_ID')
 ZOOM_CLIENT_SECRET = os.getenv('ZOOM_CLIENT_SECRET')
 
 class ZoomClient:
+        
+
     def __init__(self, account_id, client_id, client_secret) -> None:
         self.account_id = account_id
         self.client_id = client_id
         self.client_secret = client_secret
         self.access_token = self.get_access_token()
+        self.base_dir = "etl-dg/MedallionArchitecture"
+        # Nombres de las carpetas
+        self.folders = ["Bronze", "Silver", "Gold"]
+        if not os.path.exists(self.base_dir):
+            for folder in self.folders:
+                path = os.path.join(self.base_dir, folder)
+                # Comprobar si la carpeta ya existe
+                if not os.path.exists(path):
+                    os.makedirs(path)
 
     # Obtener token de acceso
     def get_access_token(self):
@@ -30,7 +42,8 @@ class ZoomClient:
     
     def __str__(self):
         return f"ZoomClient(account_id={self.account_id}, client_id={self.client_id}, client_secret={self.client_secret}, access_token={self.access_token})"
-
+    
+    # Obtiene información de reuniones mediante una fecha inicio y fecha final | No veo uso
     def get_name_meetings(self, from_date=None, to_date=None):
         headers = {
             "Authorization": f"Bearer {self.access_token}"
@@ -46,7 +59,7 @@ class ZoomClient:
             url += f"?to={to_date}"
 
         return requests.get(url, headers=headers).json()
-    
+ #------   
     # Obtener última reunión
     def get_last_meeting(self):
         headers = {
@@ -55,24 +68,22 @@ class ZoomClient:
         url = f"https://api.zoom.us/v2/users/me/meetings?type=past&page_size=1&sort_by=start_time&sort_order=desc"
         return requests.get(url, headers=headers).json()
     
-    # Obtener todas las reuniones pasadas y futuras(que se hayan programado) | Aún falta validar la paginación
-    def get_meetings_hist(self):
+    # Obtener reunión mediante una fecha de inicio y fecha de fin.
+    def get_last_meeting_date(self, from_date=None, to_date=None):
         headers = {
             "Authorization": f"Bearer {self.access_token}"
         }
-        url = f"https://api.zoom.us/v2/users/me/meetings"
+        url = "https://api.zoom.us/v2/users/me/meetings"
+        if from_date is not None or to_date is not None:
+            url += "?"
+            if from_date is not None:
+                url += f"from={from_date}"
+            if to_date is not None:
+                url += f"&to={to_date}"
 
         return requests.get(url, headers=headers).json()
     
-    # Obtener todas las reuniones pasadas
-    def get_meetings_hist_past(self):
-        headers = {
-            "Authorization": f"Bearer {self.access_token}"
-        }
-
-        url = f"https://api.zoom.us/v2/users/me/meetings?type=past"
-        return requests.get(url, headers=headers).json()
-    
+# -----
     # Obtener lista de participantes de una reunión por ID
     def get_meetings_by_id(self, meetingId):
         headers = {
@@ -95,16 +106,8 @@ class ZoomClient:
         # Convertir el JSON a un DataFrame
         df = pd.DataFrame(rq_participantes['participants'])
         # Exportar a CSV si es necesario
-        if nombre_archivo:
+        if nombre_archivo is not None:
             df.to_csv(nombre_archivo, index=False)
+        else:
+            df.to_csv(f"{self.base_dir}/{self.folders[0]}/archivo_xd.csv", index=False)
         return df
-
-
-# client = ZoomClient(account_id=ZOOM_ACCOUNT_ID, client_id=ZOOM_CLIENT_ID, client_secret=ZOOM_CLIENT_SECRET)
-#rq = client.get_name_meetings(from_date="2024-05-03", to_date="2024-05-03")
-#rq = client.get_last_meeting()
-#rq = client.get_meetings_by_id('85975843619')
-# rq = client.get_last_meeting()
-# print(rq['id'])
-""" df = pd.DataFrame(rq['participants'])
-print(df) """
